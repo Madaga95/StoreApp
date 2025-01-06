71909f5fb3b5ac87c396c504ceffd3ea7f3961d1
Madaga95 <amathgueye95@gmail.com>
2025-01-06 21:26:14 +0100

    Initial Commit

---
+++ StoreApp/StoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by Madaga on 06/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
